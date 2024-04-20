274cc547725034483fed5d118f7ff001126f3720
Piush Singh <101204142+SPYDPiush@users.noreply.github.com>
2024-04-20 19:20:02 +0000

    final touchup complet the password generator using useEffect and useCallback function

--- 03_password_generator/src/App.jsx
+++ 03_password_generator/src/App.jsx
@@ -1,31 +1,48 @@
-import { useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+
+
 
 function App() {
 
-  const [password,setPassword] = useState(null)
-  const [isnumber,setIsnumber] = useState(false)
-  const [ischar,setIschar] = useState(false)
-  const [length,setLength] = useState(8)
+
+  const clickref = useRef(null)
+
+  const [password, setPassword] = useState(null)
+  const [isnumber, setIsnumber] = useState(false)
+  const [ischar, setIschar] = useState(false)
+  const [length, setLength] = useState(8)
 
 
-
-  const passwordgenerator = () => {
-
-    let pass =""
-    const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const passwordgenerator = useCallback(() => {
+    let pass = ""
+    let letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     const number = 1234567890
     const char = '!@#$%^&*()_-+={}[]:;"><,./?|'
 
-    if(isnumber) letter+=number
-    if(ischar)  letter+=char
+    if (isnumber) letter += number;
+    if (ischar) letter += char;
 
-    for(let i; i< length ; i++){
-      let num = Math.round(Math.random() * (letter.length - 0) +1)
-      pass+=letter[num]
-      
+    for (let i = 0; i < length; i++) {
+      let num = Math.round(Math.random() * (letter.length - 2) + 1)
+      pass += letter[num]
+
     }
-    console.log(pass);
+    setPassword(pass)
 
+  }, [isnumber, ischar, length])
+
+
+  useEffect(
+    () => {
+      passwordgenerator()
+    }, [isnumber, ischar, length]
+  )
+
+  const clicktocopy = () => {
+
+    clickref.current?.select();
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.setSelectionRange(0, 999); // range define for select the input text.
   }
 
 
@@ -38,9 +55,11 @@ function App() {
             type="text"
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
+            value={password}
             readOnly
+            ref={clickref}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">Copy</button>
+          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0" onClick={clicktocopy}>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
@@ -49,19 +68,19 @@ function App() {
               min={6}
               max={100}
               value={length}
-              onChange={ (e) => setLength(e.target.value)}
+              onChange={(e) => setLength(e.target.value)}
               className='cursor-pointer' />
             <label>Length: {length}</label>
           </div>
           <div className="flex items-center gap-x-1">
             <input
-              type="checkbox" id="numberInput"/>
-              <label htmlFor="numberInput">Numbers</label>
+              type="checkbox" id="numberInput"  onChange={ () => setIsnumber( (prev) => !prev) }/> 
+            <label htmlFor="numberInput">Numbers</label>
           </div>
           <div className="flex items-center gap-x-1">
             <input
-              type="checkbox" id="numberInput"/>
-              <label htmlFor="numberInput">Numbers</label>
+              type="checkbox" id="numberInput" onChange={ () => setIschar(!ischar)} />
+            <label htmlFor="numberInput">Character</label>
           </div>
 
         </div>
