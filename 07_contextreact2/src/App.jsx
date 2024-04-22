2a41f8159dd3f410e985f2ba22bb20b5e06def09
Piush Singh <101204142+SPYDPiush@users.noreply.github.com>
2024-04-22 21:08:44 +0000

    start themechange using useContext

---
+++ 07_contextreact2/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { themeprovider } from "./context/contextapi"
+function App() {
+
+  return (
+    <themeprovider >
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+
+          </div>
+        </div>
+      </div>
+
+    </themeprovider>
+  )
+}
+
+export default App
---
+++ 07_contextreact2/src/context/contextapi.jsx
@@ -0,0 +1,16 @@
+import React , {createContext,useContext,useState} from "react";
+
+export const themecontext = createContext({
+    thememode : "dark",
+    themelight : () => {},
+    themedark : () => {},
+});
+
+export const themeprovider = themecontext.Provider
+
+export default function useTheme() {
+    return useContext(themecontext)
+} 
+
+
+
